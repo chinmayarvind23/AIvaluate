e3c530621a3fbcc10a4f158d3d2d629f8803a075
colton456p <colton.p@hotmail.com>
2024-06-09 11:27:40 -0700

    modulized the cards in dashboard

---
+++ app/aivaluate/frontend_js/src/components/card.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles.css';
+
+const Card = ({courseCode, courseName,}) => {
+  const navigate = useNavigate();
+  return (
+    <div className="course-card" onClick={() => navigate('/AssignmentOverview')}>
+        <img src="../../public/webpic.svg" alt="Course" />
+        {/* <h2>COSC 499</h2> */}
+        <h2>{courseCode}</h2>
+        {/* <p>Software Engineering Capstone</p> */}
+        <p>{courseName}</p>
+
+    </div>
+  );
+};
+
+export default Card;
--- app/aivaluate/frontend_js/src/pageComponents/Dashboard.jsx
+++ app/aivaluate/frontend_js/src/pageComponents/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AIvaluateNavBar from '../components/AIvaluateNavBar';
+import CourseCards from '../components/CourseCards';
 import '../styles.css';
 
 const Dashboard = () => {
@@ -13,7 +14,7 @@ const Dashboard = () => {
   };
 
   const clickedMenu = {
-    color: 'white',
+    color: 'white', 
     background: '#4d24d4',
     float: 'right',
     marginBottom: '10px'
@@ -28,28 +29,7 @@ const Dashboard = () => {
   return (
     <div>
       <AIvaluateNavBar navBarText='Hello Colton'  />
-      <div className="dashboard">
-        <div className="course-card" onClick={() => navigate('/AssignmentOverview')}>
-          <img src="../../public/webpic.svg" alt="Course" />
-          <h2>COSC 499</h2>
-          <p>Software Engineering Capstone</p>
-        </div>
-        <div className="course-card">
-          <img src="../../public/webpic.svg" alt="Course" />
-          <h2>COSC 360</h2>
-          <p>Intro to Web Development</p>
-        </div>
-        <div className="course-card">
-          <img src="../../public/webpic.svg" alt="Course" />
-          <h2>COSC 304</h2>
-          <p>Web Development II</p>
-        </div>
-        <div className="course-card">
-          <img src="../../public/webpic.svg" alt="Course" />
-          <h2>COSC 395</h2>
-          <p>Intro to Frontend Development</p>
-        </div>
-      </div>
+      <CourseCards />
     </div>
   );
 };

ed7baea92fd9015fe7c32d93b26878ba4bda71a9
Omar Hemed <omarhemed1@hotmail.com>
2024-06-12 20:35:08 -0700

    Worked on the account page and fixed the edit issue

--- app/aivaluate/frontend/src/pageComponents/Account.jsx
+++ app/aivaluate/frontend/src/pageComponents/Account.jsx
@@ -1,67 +1,147 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../Account.css';
 import '../GeneralStyling.css';
 import AIvaluateNavBar from '../components/AIvaluateNavBar';
 import '../styles.css';
 
 const Account = () => {
-    const firstName = "Colton";
-    const lastName = "Palfrey";
-    const email = "colton@email.com";
-    const password = "*********";
-    const accountId = "12348817";
-    var id = "Student ID";
-    const prof = false;
-    if (prof === true) {
-        id = "Evaluator ID";
+    const [user, setUser] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        studentId: ''
+    });
+
+    const [editableFields, setEditableFields] = useState({
+        firstName: false,
+        lastName: false,
+        email: false,
+        password: false
+    });
+
+    useEffect(() => {
+        fetch('/users/me')
+            .then(response => response.json())
+            .then(data => setUser(data));
+    }, []);
+
+    const handleEdit = (field, newValue) => {
+        setUser(prevUser => ({
+            ...prevUser,
+            [field]: newValue
+        }));
     }
 
-    const editFirstName = () => {
-
+    const handleEditClick = (field) => {
+        setEditableFields(prevEditableFields => ({
+            ...prevEditableFields,
+            [field]: true
+        }));
     }
 
-    const editLastName = () => {
-        
-    }
-    const editEmail = () => {
-        
-    }
-    const editPassword = () => {
-        
+    const handleSave = (field) => {
+        setEditableFields(prevEditableFields => ({
+            ...prevEditableFields,
+            [field]: false
+        }));
+        // Here you can add any logic to save the changes, like making a fetch request
     }
 
-  return (
-    <div class="background-colour">
-        <AIvaluateNavBar navBarText='Your Account' tab='account'/>  
-            <div className="fourth-colorbg account-details">
-                <div className="detail-label">First Name</div>
-                <div className="detail-row">
-                    <div className="primary-colorbg detail-value">{firstName}</div>
-                    <button action={firstName} className="primary-button edit-button">Edit</button>
+    return (
+        <div className="background-colour">
+            <AIvaluateNavBar navBarText='Your Account' tab='account'/>  
+                <div className="fourth-colorbg account-details">
+                    <div className="detail-label">First Name</div>
+                    <div className="detail-row">
+                        {editableFields.firstName ? (
+                            <input 
+                                className="primary-colorbg detail-value"
+                                type="text"
+                                value={user.firstName}
+                                onChange={(e) => handleEdit('firstName', e.target.value)}
+                                onBlur={() => handleSave('firstName')}
+                            />
+                        ) : (
+                            <div 
+                                className="primary-colorbg detail-value"
+                                onClick={() => handleEditClick('firstName')}
+                            >
+                                {user.firstName}
+                            </div>
+                        )}
+                        {editableFields.firstName && <button onClick={() => handleSave('firstName')}>Save</button>}
+                    </div>
+                    <div className="detail-label">Last Name</div>
+                    <div className="detail-row">
+                        {editableFields.lastName ? (
+                            <input 
+                                className="primary-colorbg detail-value"
+                                type="text"
+                                value={user.lastName}
+                                onChange={(e) => handleEdit('lastName', e.target.value)}
+                                onBlur={() => handleSave('lastName')}
+                            />
+                        ) : (
+                            <div 
+                                className="primary-colorbg detail-value"
+                                onClick={() => handleEditClick('lastName')}
+                            >
+                                {user.lastName}
+                            </div>
+                        )}
+                        {editableFields.lastName && <button onClick={() => handleSave('lastName')}>Save</button>}
+                    </div>
+                    <div className="detail-label">Email</div>
+                    <div className="detail-row">
+                        {editableFields.email ? (
+                            <input 
+                                className="primary-colorbg detail-value"
+                                type="email"
+                                value={user.email}
+                                onChange={(e) => handleEdit('email', e.target.value)}
+                                onBlur={() => handleSave('email')}
+                            />
+                        ) : (
+                            <div 
+                                className="primary-colorbg detail-value"
+                                onClick={() => handleEditClick('email')}
+                            >
+                                {user.email}
+                            </div>
+                        )}
+                        {editableFields.email && <button onClick={() => handleSave('email')}>Save</button>}
+                    </div>
+                    <div className="detail-label">Password</div>
+                    <div className="detail-row">
+                        {editableFields.password ? (
+                            <input 
+                                className="primary-colorbg detail-value"
+                                type="password"
+                                value={user.password}
+                                onChange={(e) => handleEdit('password', e.target.value)}
+                                onBlur={() => handleSave('password')}
+                            />
+                        ) : (
+                            <div 
+                                className="primary-colorbg detail-value"
+                                onClick={() => handleEditClick('password')}
+                            >
+                                {user.password}
+                            </div>
+                        )}
+                        {editableFields.password && <button onClick={() => handleSave('password')}>Save</button>}
+                    </div>
+                    <div className="detail-label">Student ID</div>
+                    <div className="detail-row">
+                        <div className="primary-colorbg detail-value">{user.studentId}</div>
+                    </div>
                 </div>
-                <div className="detail-label">Last Name</div>
-                <div className="detail-row">
-                    <div className="primary-colorbg detail-value">{lastName}</div>
-                    <button action={lastName} className="primary-button edit-button">Edit</button>
-                </div>
-                <div className="detail-label">Email</div>
-                <div className="detail-row">
-                    <div className="primary-colorbg detail-value">{email}</div>
-                    <button action={editEmail} className="primary-button edit-button">Edit</button>
-                </div>
-                <div className="detail-label">Password</div>
-                <div className="detail-row">
-                    <div className="primary-colorbg detail-value">{password}</div>
-                    <button action={editPassword} className="primary-button edit-button">Edit</button>
-                </div>
-                <div className="detail-label">{id}</div>
-                <div className="detail-row">
-                    <div className="primary-colorbg detail-value">{accountId}</div>
-                </div>
-            </div>
-    </div>
-    
-  );
+        </div>
+    );
 };
 
 export default Account;
+
+
+

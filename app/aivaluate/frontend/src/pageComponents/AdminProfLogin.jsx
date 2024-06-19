ca4f6d3f5c55d60e7147cc6586c5fee29873f72a
AAYUSH CHAUDHARY <aayushchaudhary84@gmail.com>
2024-06-19 10:28:49 -0700

    done

--- app/aivaluate/frontend/src/pageComponents/AdminProfLogin.jsx
+++ app/aivaluate/frontend/src/pageComponents/AdminProfLogin.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+//import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../Auth.css';
+
 
 const AdminProfLogin = () => {
   const navigate = useNavigate();

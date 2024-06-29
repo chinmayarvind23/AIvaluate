14370ba36102adb7252f165c62d33ad286576885
jcfan91 <jcfan91@hotmail.com>
2024-06-29 00:45:51 -0700

    Fixed an error where user cannot log in Eval dashboard

--- app/aivaluate/backend-eval/routes/evalRoutes.js
+++ app/aivaluate/backend-eval/routes/evalRoutes.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const router = express.Router();
-const passport = require('passport');
+// const express = require('express');
+// const router = express.Router();
+// const passport = require('passport');
 
 // Instructor login route
 // router.post('/login', passport.authenticate('local', {
@@ -9,14 +9,25 @@ const passport = require('passport');
 //     failureFlash: true
 // }));
 
-// Instructor dashboard route
-router.get('/dashboard', (req, res) => {
-    res.send('Evaluator Dashboard');
-});
+// function checkAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next(); // User is authenticated, continue to the next middleware
+//     }
+//     // Redirect to login page only if not already on the login page
+//     if (req.originalUrl !== '/eval-api/login') {
+//         return res.redirect('/eval-api/login');
+//     }
+//     next(); // Continue to the next middleware if already on the login page
+// }
+
+// // Instructor dashboard route
+// router.get('/dashboard', checkAuthenticated, (req, res) => {
+//     res.send('Evaluator Dashboard');
+// });
 
 // // Instructor login page route
 // router.get('/login', (req, res) => {
 //     res.send('Evaluator Login Page');
 // });
 
-module.exports = router;
+// module.exports = router;

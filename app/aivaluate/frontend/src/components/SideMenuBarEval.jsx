4716bbaa2b67366760276291b990b12e01b0775a
jcfan91 <jcfan91@hotmail.com>
2024-07-02 07:39:19 -0700

    Eval Container Dynamic Name Display

--- app/aivaluate/frontend/src/components/SideMenuBarEval.jsx
+++ app/aivaluate/frontend/src/components/SideMenuBarEval.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../GeneralStyling.css';
 import '../SideMenu.css';
+import { Link } from 'react-router-dom'; // to prevent page reload
 
  const SideMenuBarEval = ({tab}) => {
     const courseId = sessionStorage.getItem('courseId');
@@ -8,12 +9,12 @@ import '../SideMenu.css';
 
  return (
          <div className="fourth-colorbg side-menu rborder">
-             <a href={`/eval/coursehome/${courseId}`} className={`${tab === "home" ? 'primary-color-text' : 'third-color-text'}`}>Management</a>
-             <a href="/eval/grades" className={`${tab === "grades" ? 'primary-color-text' : 'third-color-text'}`}>Student Grades</a>
-             <a href="/eval/grading" className={`${tab === "assignments" ? 'primary-color-text' : 'third-color-text'}`}>Assignments</a>
-             <a href="/eval/students" className={`${tab === "students" ? 'primary-color-text' : 'third-color-text'}`}>Students</a>
-             <a href="/eval/submissions" className={`${tab === "submissions" ? 'primary-color-text' : 'third-color-text'}`}>All Submissions</a>
-             <a href="/eval/rubrics" className={`${tab === "rubrics" ? 'primary-color-text' : 'third-color-text'}`}>Rubrics</a>
+            <Link to={`/eval/coursehome/${courseId}`} className={`${tab === "home" ? 'primary-color-text' : 'third-color-text'}`}>Management</Link>
+             <Link to="/eval/grades" className={`${tab === "grades" ? 'primary-color-text' : 'third-color-text'}`}>Student Grades</Link>
+             <Link to="/eval/grading" className={`${tab === "assignments" ? 'primary-color-text' : 'third-color-text'}`}>Assignments</Link>
+             <Link to="/eval/students" className={`${tab === "students" ? 'primary-color-text' : 'third-color-text'}`}>Students</Link>
+             <Link to="/eval/submissions" className={`${tab === "submissions" ? 'primary-color-text' : 'third-color-text'}`}>All Submissions</Link>
+             <Link to="/eval/rubrics" className={`${tab === "rubrics" ? 'primary-color-text' : 'third-color-text'}`}>Rubrics</Link>
          </div>
  )
  }

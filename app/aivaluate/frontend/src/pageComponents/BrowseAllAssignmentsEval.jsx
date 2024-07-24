2d0bdf0b208dc16dbeb288849a03cf986ceca56e
AAYUSH CHAUDHARY <aayushchaudhary84@gmail.com>
2024-07-23 22:50:59 -0700

    update

--- app/aivaluate/frontend/src/pageComponents/BrowseAllAssignmentsEval.jsx
+++ app/aivaluate/frontend/src/pageComponents/BrowseAllAssignmentsEval.jsx
@@ -161,3 +161,4 @@ const BrowseAllAssignmentsEval = () => {
 };
 
 export default BrowseAllAssignmentsEval;
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>

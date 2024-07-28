23ace281b179e8a9d0e9be3eac79242540246361
Chinmay Arvind <chinmayarvind23@gmail.com>
2024-07-28 05:09:42 -0700

    Submit assignment button fixed on frontend

--- app/aivaluate/frontend/src/pageComponents/SubmitAssignment.jsx
+++ app/aivaluate/frontend/src/pageComponents/SubmitAssignment.jsx
@@ -1,6 +1,6 @@
 import CircumIcon from "@klarr-agency/circum-icons-react";
 import axios from 'axios';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import '../GeneralStyling.css';
 import '../SubmitAssignment.css';
@@ -11,10 +11,10 @@ import SideMenuBar from '../components/SideMenuBar';
 const SubmitAssignment = () => {
     const courseCode = sessionStorage.getItem('courseCode');
     const courseName = sessionStorage.getItem('courseName');
+    const navBarText = `${courseCode} - ${courseName}`;
 
     const navigate = useNavigate();
     const { courseId, assignmentId } = useParams();
-    const navBarText = `${courseCode} - ${courseName}`;
     const [files, setFiles] = useState([]);
     const [isGraded, setIsGraded] = useState(false);
     const [dragging, setDragging] = useState(false);
@@ -83,29 +83,29 @@ const SubmitAssignment = () => {
         const selectedFiles = Array.from(e.dataTransfer.files);
         handleFileChange({ target: { files: selectedFiles } });
     };
-    
+
     const handleDragOver = (e) => {
         e.preventDefault();
         setDragging(true);
     };
-    
+
     const handleDragLeave = () => {
         setDragging(false);
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
         if (files.length === 0) {
             setErrorMessage('Please select files to upload.');
             return;
         }
-    
+
         const formData = new FormData();
         files.forEach(file => {
             formData.append('files', file);
         });
-    
+
         try {
             await axios.post(`/stu-api/upload/${courseId}/${assignmentId}`, formData, {
                 headers: {
@@ -119,19 +119,17 @@ const SubmitAssignment = () => {
             console.error('File upload failed:', err);
             setErrorMessage('File upload failed. Please try again.');
         }
-    };    
+    };
 
     if (loading) {
         return <div>Loading...</div>;
     }
 
-    // const navBarText = `${assignmentDetails.assignmentName} - ${assignmentDetails.assignmentDescription}`;
-
     return (
         <div>
             <AIvaluateNavBar navBarText={navBarText} />
             <div className="filler-div">
-                <SideMenuBar tab="assignments"/>
+                <SideMenuBar tab="assignments" />
                 <div className="main-margin">
                     <div className="assignment-container secondary-colorbg">
                         <div className="top-bar">
@@ -154,90 +152,84 @@ const SubmitAssignment = () => {
                                     <h3>Score: {assignmentDetails.InstructorAssignedFinalGrade}/{assignmentDetails.maxObtainableGrade}</h3>
                                 </div>
                             </div>
-                            
-                            <div className="file-upload">
-                                <form onSubmit={handleSubmit}>
+                            <form onSubmit={handleSubmit}>
+                                <div className={`file-upload ${dragging ? 'dragging' : ''}`}
+                                    onDragOver={handleDragOver}
+                                    onDragLeave={handleDragLeave}
+                                    onDrop={handleDrop}>
                                     <label htmlFor="file-upload" className="file-upload-label">
-                                            Drag files here or Click to browse files
-                                        </label>
-                                    <div
-                                        className={`file-upload ${dragging ? 'dragging' : ''}`}
-                                        onDragOver={handleDragOver}
-                                        onDragLeave={handleDragLeave}
-                                        onDrop={handleDrop}
-                                    >
-                                        
-                                        <input 
-                                            type="file" 
-                                            id="file-upload" 
-                                            className="file-upload-input" 
-                                            name="files"
-                                            onChange={handleFileChange}
-                                            multiple 
-                                        />
-                                    </div>
-                                    {errorMessage && <p className="error-message">{errorMessage}</p>}
-                                    <div className="submit-right">
-                                        <h2 className="assignment-text">Assignment Details</h2>
-                                        <div className="empty"> </div>
-                                        <button className="submit-button rborder" type="submit">Submit</button>
-                                    </div>
-                                </form>
-                                <div className="assignment-details">
-                                    <pre className="details-content">{assignmentDetails.criteria}</pre>
+                                        Drag files here or Click to browse files
+                                    </label>
+                                    <input
+                                        type="file"
+                                        id="file-upload"
+                                        className="file-upload-input"
+                                        name="files"
+                                        onChange={handleFileChange}
+                                        multiple
+                                    />
                                 </div>
-                                <h2>Files to be uploaded</h2>
-                                <div className="uploaded-files-container">
-                                    {files.length > 0 ? (
-                                        <ul>
-                                            {files.map((file, index) => (
+                                {errorMessage && <p className="error-message">{errorMessage}</p>}
+                                <div className="submit-right">
+                                    <h2 className="assignment-text">Assignment Details</h2>
+                                    <div className="empty"> </div>
+                                    <button className="submit-button rborder" type="submit">Submit</button>
+                                </div>
+                            </form>
+                            <div className="assignment-details">
+                                <pre className="details-content">{assignmentDetails.criteria}</pre>
+                            </div>
+                            <h2>Files to be uploaded</h2>
+                            <div className="uploaded-files-container">
+                                {files.length > 0 ? (
+                                    <ul>
+                                        {files.map((file, index) => (
+                                            <li key={index}>
+                                                <span>{file.name}</span>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p>No files selected yet.</p>
+                                )}
+                            </div>
+                            <h2>Recently Uploaded Files</h2>
+                            <div className="uploaded-files-container">
+                                {uploadedFiles.length > 0 ? (
+                                    <ul>
+                                        {uploadedFiles.flatMap(submission => (
+                                            Array.isArray(submission.files) ? submission.files.map((file, index) => (
                                                 <li key={index}>
-                                                    <span>{file.name}</span>
+                                                    <a href={`/${file}`} target="_blank" rel="noopener noreferrer">{String(file).split('/').pop()}</a>
                                                 </li>
-                                            ))}
-                                        </ul>
-                                    ) : (
-                                        <p>No files selected yet.</p>
-                                    )}
-                                </div>
-                                <h2>Recently Uploaded Files</h2>
-                                <div className="uploaded-files-container">
-                                    {uploadedFiles.length > 0 ? (
-                                        <ul>
-                                            {uploadedFiles.flatMap(submission => (
-                                                Array.isArray(submission.files) ? submission.files.map((file, index) => (
-                                                    <li key={index}>
-                                                        <a href={`/${file}`} target="_blank" rel="noopener noreferrer">{String(file).split('/').pop()}</a>
-                                                    </li>
-                                                )) : null
-                                            ))}
-                                        </ul>
-                                    ) : (
-                                        <p>No files uploaded yet.</p>
-                                    )}
-                                </div>
-                                <h2>Feedback</h2>
-                                <div className="feedback-container">
-                                    {isGraded ? (
-                                        <div className="feedback">
-                                            <div className="score-class">
-                                                <div className="empty"> </div>
+                                            )) : null
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p>No files uploaded yet.</p>
+                                )}
+                            </div>
+                            <h2>Feedback</h2>
+                            <div className="feedback-container">
+                                {isGraded ? (
+                                    <div className="feedback">
+                                        <div className="score-class">
+                                            <div className="empty"> </div>
+                                        </div>
+                                        <div className="both-feedback">
+                                            <h3>AI Feedback</h3>
+                                            <div className="feeback-text">
+                                                <MarkdownRenderer markdownText={assignmentDetails.aiFeedback || ''} />
                                             </div>
-                                            <div className="both-feedback">
-                                                <h3>AI Feedback</h3>
-                                                <div className="feeback-text">
-                                                    <MarkdownRenderer markdownText={assignmentDetails.aiFeedback || ''} />
-                                                </div>
-                                                <h3>Evaluator Feedback</h3>
-                                                <div className="feeback-text">
-                                                    <MarkdownRenderer markdownText={assignmentDetails.evaluatorFeedback || ''} />
-                                                </div>
+                                            <h3>Evaluator Feedback</h3>
+                                            <div className="feeback-text">
+                                                <MarkdownRenderer markdownText={assignmentDetails.evaluatorFeedback || ''} />
                                             </div>
                                         </div>
-                                    ) : (
-                                        <h3>No feedback available yet...</h3>
-                                    )}
-                                </div>
+                                    </div>
+                                ) : (
+                                    <h3>No feedback available yet...</h3>
+                                )}
                             </div>
                         </div>
                     </div>

b3b99d5c1536c529c18dc62f3386ee61c9e4d273
jcfan91 <jcfan91@hotmail.com>
2024-07-28 18:18:35 -0700

    Admin and Student Account Page Ui Fix

--- app/aivaluate/frontend/src/pageComponents/StudentAccount.jsx
+++ app/aivaluate/frontend/src/pageComponents/StudentAccount.jsx
@@ -167,14 +167,14 @@ const StudentAccount = () => {
                                     value={confirmNewPassword}
                                     onChange={(e) => setConfirmNewPassword(e.target.value)}
                                 />
-                                <button className="primary-button save-button" onClick={handlePasswordSaveClick}>Save</button>
+                                <button className="primary-button account-save-button" onClick={handlePasswordSaveClick}>Save</button>
                             </div>
                             {errorMessage && <div className="error-message">{errorMessage}</div>}
                         </>
                     ) : (
                         <div className="password-edit-container">
                             <div className="primary-colorbg password-input">**********</div>
-                            <button className="primary-button edit-button" onClick={handlePasswordEditClick}>Edit</button>
+                            <button className="primary-button account-edit-button" onClick={handlePasswordEditClick}>Edit</button>
                         </div>
                     )}
                     {successMessage && <div className="success-message">{successMessage}</div>}

2dbba23587e184f395ff89bb803fca8c5edf6fc0
colton456p <colton.p@hotmail.com>
2024-07-29 21:41:39 -0700

    more fixes to all frontends

--- app/aivaluate/frontend/src/pageComponents/EvalLogin.jsx
+++ app/aivaluate/frontend/src/pageComponents/EvalLogin.jsx
@@ -54,7 +54,7 @@ const EvalLogin = () => {
   };
 
   return (
-    <div className="background">
+    <div className="background-div">
       <div className="logo">
         <div className="logoText">
           <h1 className="primary-color-text">AI</h1><h1 className="third-color-text">valuate</h1>

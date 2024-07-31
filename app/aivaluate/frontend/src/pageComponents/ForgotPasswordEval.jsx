89fa8f44c2aa5ea8a2713fd1507db682dff65986
colton456p <colton.p@hotmail.com>
2024-07-31 12:56:36 -0700

    fixed forgot password pages

--- app/aivaluate/frontend/src/pageComponents/ForgotPasswordEval.jsx
+++ app/aivaluate/frontend/src/pageComponents/ForgotPasswordEval.jsx
@@ -43,7 +43,7 @@ const ForgotPasswordEval = () => {
     <div className="background">
       <div className="logo">
         <div className="logoText">
-          <h1 className="primary-color-text">AI</h1><h1 className="secondary-color-text">valuate</h1>
+          <h1 className="primary-color-text">AI</h1><h1 className="third-color-text">valuate</h1>
         </div>
       </div>
       <div className="auth-container">

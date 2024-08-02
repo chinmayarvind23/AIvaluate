828863af707b906a9d86da7d832aa3c182f63fee
AAYUSH CHAUDHARY <aayushchaudhary84@gmail.com>
2024-08-01 21:57:20 -0700

    updated coverage

--- app/aivaluate/frontend/babel-jest-transformer.cjs
+++ app/aivaluate/frontend/babel-jest-transformer.cjs
@@ -2,6 +2,7 @@ const babelJest = require('babel-jest');
 
 module.exports = babelJest.createTransformer({
   presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: ['istanbul'],
   babelrc: false,
   configFile: false,
 });
